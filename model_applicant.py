c388adbf7dfeff31aeab2272420a9406e2f864bb
oliver <kiss.oliver.attila@gmail.com>
2016-07-20 14:54:04 +0200

    application_code_generator implemented

--- model_applicant.py
+++ model_applicant.py
@@ -1,5 +1,6 @@
 from model_base import *
 from peewee import *
+from random import randint
 
 
 class Applicant(BaseModel):
@@ -12,4 +13,11 @@ class Applicant(BaseModel):
     @classmethod
     def new_applicant(cls):
         object_list = list(cls.select().where(cls.application_code.is_null(True)))
-        return(object_list)
+        return object_list
+
+    @classmethod
+    def application_code_generator(cls):
+        object_list = cls.new_applicant()
+        for element in object_list:
+            element.application_code = randint(10000, 99999)
+            element.save()
