7f4212c01a58f07ac46a3c02a0c26580d642a9ad
Marchello92 <marton.kovacs1992@gmail.com>
2016-07-20 14:01:54 +0200

    new_applicant method implemented

--- model_applicant.py
+++ model_applicant.py
@@ -1,9 +1,15 @@
 from model_base import *
+from peewee import *
 
 
 class Applicant(BaseModel):
     name = CharField()
     city = CharField()
-    application_code = IntegerField(default=None)
-    school = CharField(default=None)
+    application_code = IntegerField(null=True)
+    school = CharField(null=True)
     status = CharField(default="new")
+
+    @classmethod
+    def new_applicant(cls):
+        object_list = list(cls.select().where(cls.application_code.is_null(True)))
+        return(object_list)
