9c2c2f77958d1f806e8d56b0e4e77b3ed2c6f72c
Benedek Vörös <xbenedek01@gmail.com>
2016-07-20 12:05:20 +0200

    creation of example data

--- example_data.py
+++ example_data.py
@@ -1,3 +1,33 @@
 # This script can generate example data for "City" and "InterviewSlot" models.
+from model_city import *
+from model_school import *
 
-from models import *
+
+# generating schools
+ccbudapest = School.create(name="CodecoolBudapest")
+ccmiskolc = School.create(name="CodecoolMiskolc")
+cckrakow = School.create(name="CodecoolKrakow")
+
+# generating cities
+City.create(name="Kaposvár", school=ccbudapest)
+City.create(name="Érd", school=ccbudapest)
+City.create(name="Visegrád", school=ccbudapest)
+City.create(name="Veszprém", school=ccbudapest)
+City.create(name="Sopron", school=ccbudapest)
+City.create(name="Kecskemét", school=ccbudapest)
+City.create(name="Győr", school=ccbudapest)
+City.create(name="Gödöllő", school=ccbudapest)
+City.create(name="Hatvan", school=ccbudapest)
+City.create(name="Vác", school=ccbudapest)
+City.create(name="Eger", school=ccmiskolc)
+City.create(name="Nyíregyháza", school=ccmiskolc)
+City.create(name="Debrecen", school=ccmiskolc)
+City.create(name="Kiskőrös", school=ccmiskolc)
+City.create(name="Gyula", school=ccmiskolc)
+City.create(name="Hajdúszoboszló", school=ccmiskolc)
+City.create(name="Kassa", school=ccmiskolc)
+City.create(name="Wieliczka", school=cckrakow)
+City.create(name="Liszki", school=cckrakow)
+City.create(name="Katowice", school=cckrakow)
+City.create(name="Varsó", school=cckrakow)
+City.create(name="Berlin", school=cckrakow)
