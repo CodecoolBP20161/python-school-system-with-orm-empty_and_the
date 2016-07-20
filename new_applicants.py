f79e930615da6056832ba673b05e1f6dee7d87e9
Benedek Vörös <xbenedek01@gmail.com>
2016-07-20 15:06:45 +0200

    names changed, first_name, last_name added

--- new_applicants.py
+++ new_applicants.py
@@ -3,14 +3,15 @@
 
 from model_base import *
 from model_applicant import *
+from example_data import *
 
-Applicant.create(name="Smith, John", city="Varsó")
-Applicant.create(name="Smith, Jane", city="Liszki")
-Applicant.create(name="Smith, Johnny", city="Berlin")
-Applicant.create(name="Kovács, János", city="Nyíregyháza")
-Applicant.create(name="Molnár, László", city="Debrecen")
-Applicant.create(name="Szűcs, Antal", city="Győr")
-Applicant.create(name="Kocsis, Mária", city="Hatvan")
-Applicant.create(name="Monthy, Python", city="Kassa")
-Applicant.create(name="Szabó, Ilona", city="Gödöllő")
-Applicant.create(name="Tóth, Lajos", city="Veszprém")
+Applicant.create(first_name="Smith", last_name="John", city=city_varso)
+Applicant.create(first_name="Smith", last_name="Jane", city=city_liszki)
+Applicant.create(first_name="Smith", last_name="Johnny", city=city_berlin)
+Applicant.create(first_name="Kovács", last_name="János", city=city_debrecen)
+Applicant.create(first_name="Molnár", last_name="László", city=city_debrecen)
+Applicant.create(first_name="Szűcs", last_name="Antal", city=city_gyor)
+Applicant.create(first_name="Kocsis", last_name="Mária", city=city_hatvan)
+Applicant.create(first_name="Monthy", last_name="Python", city=city_hatvan)
+Applicant.create(first_name="Szabó", last_name="Ilona", city=city_sopron)
+Applicant.create(first_name="Tóth", last_name="Lajos", city=city_sopron)
