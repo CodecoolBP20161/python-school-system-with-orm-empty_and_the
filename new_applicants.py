9d5e52fc4d49a88a629303cfc4e1076af0865392
Vivoca <you@example.com>
2016-07-20 13:26:06 +0200

    new applicants test data created

--- new_applicants.py
+++ new_applicants.py
@@ -1,4 +1,16 @@
 # New applicants arrive into your project database by this script.
 # You can run it anytime to generate new data!
 
-from models import *
+from model_base import *
+from model_applicant import *
+
+Applicant.create(name="Smith, John", city="Varsó")
+Applicant.create(name="Smith, Jane", city="Liszki")
+Applicant.create(name="Smith, Johnny", city="Berlin")
+Applicant.create(name="Kovács, János", city="Nyíregyháza")
+Applicant.create(name="Molnár, László", city="Debrecen")
+Applicant.create(name="Szűcs, Antal", city="Győr")
+Applicant.create(name="Kocsis, Mária", city="Hatvan")
+Applicant.create(name="Monthy, Python", city="Kassa")
+Applicant.create(name="Szabó, Ilona", city="Gödöllő")
+Applicant.create(name="Tóth, Lajos", city="Veszprém")
