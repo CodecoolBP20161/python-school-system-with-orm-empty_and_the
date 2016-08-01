8713f761a80b463fc59bb6d435eb5a7519598277
vbenedek <xbenedek01@gmail.com>
2016-08-01 16:18:27 +0200

    combine generating data into example_data.py

--- example_data.py
+++ example_data.py
@@ -1,6 +1,10 @@
-# This script can generate example data for "City" and "InterviewSlot" models.
-from model_city import *
+# This script drops all tables and generates news with some data for testing
 
+# import build for drop and create tables
+from build import *
+
+# import needed libraries
+from datetime import datetime
 
 # generating schools
 class_budapest = School.create(name="CodecoolBudapest")
@@ -30,3 +34,73 @@ city_liszki = City.create(name="Liszki", school=class_krakow)
 city_katowice = City.create(name="Katowice", school=class_krakow)
 city_varso = City.create(name="Varsó", school=class_krakow)
 city_berlin = City.create(name="Berlin", school=class_krakow)
+
+# generating mentors
+danimentor = Mentor.create(first_name="Dániel", last_name="Salamon", school=class_budapest, mentor_password=11111)
+mikimentor = Mentor.create(first_name="Miklós", last_name="Beöthy", school=class_budapest, mentor_password=22222)
+tomimentor = Mentor.create(first_name="Tamás", last_name="Tompa", school=class_budapest, mentor_password=33333)
+miskolcmentor = Mentor.create(first_name="Mihály", last_name="Miskolc", school=class_miskolc, mentor_password=44444)
+krakowmentor = Mentor.create(first_name="Károly", last_name="Krakow", school=class_krakow, mentor_password=55555)
+
+# generate new applicants without application_code
+Applicant.create(first_name="Smith", last_name="John", city=city_varso)
+Applicant.create(first_name="Smith", last_name="Jane", city=city_liszki)
+Applicant.create(first_name="Smith", last_name="Johnny", city=city_berlin)
+Applicant.create(first_name="Kovács", last_name="János", city=city_debrecen)
+Applicant.create(first_name="Molnár", last_name="László", city=city_debrecen)
+
+# generate applicants with given parameters for testing
+antal = Applicant.create(
+    first_name="Szűcs",
+    last_name="Antal",
+    city=city_gyor,
+    application_code=11111,
+    school=class_budapest,
+    status="in progress")
+maria = Applicant.create(
+    first_name="Kocsis",
+    last_name="Mária",
+    city=city_hatvan,
+    application_code=22222,
+    school=class_budapest,
+    status="in progress")
+python = Applicant.create(
+    first_name="Monthy",
+    last_name="Python",
+    city=city_hatvan,
+    application_code=33333,
+    school=class_miskolc,
+    status="in progress")
+ilona = Applicant.create(
+    first_name="Szabó",
+    last_name="Ilona",
+    city=city_sopron,
+    application_code=44444,
+    school=class_krakow,
+    status="in progress")
+lajos = Applicant.create(
+    first_name="Tóth",
+    last_name="Lajos",
+    city=city_sopron,
+    application_code=55555,
+    school=class_krakow,
+    status="in progress")
+
+# generate questions
+question1 = Question.create(question_text="2 + 2 = ?", question_status="answered", applicant=antal)
+question2 = Question.create(question_text="5 * 6 = ?", question_status="new", applicant=maria)
+question3 = Question.create(question_text="3 - 7 = ?", question_status="waiting for answer", applicant=maria)
+question4 = Question.create(question_text="15 * 3 = ?", question_status="new", applicant=maria)
+question5 = Question.create(question_text="16 / 4 = ?", question_status="answered", applicant=ilona)
+question6 = Question.create(question_text="10 * 10 = ?", question_status="answered", applicant=lajos)
+
+# generate answers
+answer1 = Answer.create(answer_text="4", question=question1)
+answer1 = Answer.create(answer_text="4", question=question5)
+answer1 = Answer.create(answer_text="100", question=question6)
+
+# generate interviews
+Interview.create(date_time=datetime(2016, 12, 6, 15, 10), mentor=mikimentor, applicant=antal)
+Interview.create(date_time=datetime(2016, 8, 7, 13, 20), mentor=danimentor, applicant=maria)
+Interview.create(date_time=datetime(2016, 10, 5, 16, 40), mentor=danimentor, applicant=ilona)
+Interview.create(date_time=datetime(2016, 9, 4, 18, 50), mentor=krakowmentor, applicant=lajos)
