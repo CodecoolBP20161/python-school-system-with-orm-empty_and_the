7aeb0164fd01a6f7c256368f46d960ca05068eef
Vivoca <you@example.com>
2016-08-01 15:19:05 +0200

    db name changed to default

--- model_base.py
+++ model_base.py
@@ -3,7 +3,7 @@ from peewee import *
 # Configure your database connection here
 # database name = should be your username on your laptop
 # database user = should be your username on your laptop
-db = PostgresqlDatabase('vbenedek', user='vbenedek')
+db = PostgresqlDatabase('dbname', user='dbuser')
 
 
 class BaseModel(Model):
