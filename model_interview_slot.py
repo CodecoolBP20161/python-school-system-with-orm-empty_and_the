6f8a995b3ad0b36f7f7e249d023e66d60cf9f756
egyud_v <vivien.egyud@gmail.com>
2016-08-02 15:27:47 +0200

    Find not reserved mentors ok

--- model_interview_slot.py
+++ model_interview_slot.py
@@ -7,4 +7,20 @@ class Interview_slot(BaseModel):
     start = DateTimeField()
     end = DateTimeField()
     reserved = BooleanField()
-    mentor = ForeignKeyField(Mentor)
+    mentor = ForeignKeyField(Mentor, related_name="mentor")
+
+    # Return the list of the slots not reserved
+    @classmethod
+    def get_mentor_object_list_not_reserved(cls):
+        interview_slot_object_list = list(cls.select().where(cls.reserved == False))
+        return interview_slot_object_list
+
+    # Find two not reserved mentor in the applicant's school and return interview start, mentor_1, mentor_2
+    @classmethod
+    def get_free_mentor_pair(cls, object_list):
+        interview_slot_object_list = cls.get_mentor_object_list_not_reserved
+        for element in interview_slot_object_list:
+            if element.mentor.school.name == school:
+                for element2 in interview_slot_object_list:
+                    if (element2.mentor.id != element.mentor.id) and (element2.start == element.start):
+                        return element.start, element.mentor.first_name, element2.mentor.first_name
