840a7de4954192450b5404b20a54877bb5141464
vbenedek <xbenedek01@gmail.com>
2016-08-03 13:32:30 +0200

    changed drop to drop cascaded

--- build.py
+++ build.py
@@ -11,5 +11,5 @@ from model_interview_slot import *
 
 db.connect()
 # List the tables here what you want to create...
-db.drop_tables([Applicant, City, School, Interview, Question, Answer, Mentor, InterviewSlot], safe=True)
+db.drop_tables([Applicant, City, School, Interview, Question, Answer, Mentor, InterviewSlot], safe=True, cascade=True)
 db.create_tables([Applicant, City, School, Interview, Question, Answer, Mentor, InterviewSlot], safe=True)
