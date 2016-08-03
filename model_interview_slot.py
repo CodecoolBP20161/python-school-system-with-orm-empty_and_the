7b98235a104a37bffb16573d569c6726e1f2e93d
vbenedek <xbenedek01@gmail.com>
2016-08-03 11:19:37 +0200

    get_interview_for_applicants classmethod implemented

--- model_interview_slot.py
+++ model_interview_slot.py
@@ -1,11 +1,10 @@
 # Interview_slot table contains the mentor's free time for the interviews
-
 from model_mentor import *
 from model_applicant import *
 from model_interview import *
 
 
-class Interview_slot(BaseModel):
+class Interview_Slot(BaseModel):
     start = DateTimeField()
     end = DateTimeField()
     reserved = BooleanField()
@@ -14,14 +13,14 @@ class Interview_slot(BaseModel):
     # Return the list of the slots not reserved
     @classmethod
     def get_mentor_object_list_not_reserved(cls):
-        interview_slot_object_list = list(cls.select().where(cls.reserves == False))
-        print(len(interview_slot_object_list))
+        # used subpress warning(# noqa) because issue: https://github.com/coleifer/peewee/issues/612
+        interview_slot_object_list = list(cls.select().where(cls.reserved == False))  # noqa
         return interview_slot_object_list
 
     # Find two not reserved mentor in the applicant's school and return interview start, mentor_1, mentor_2
     @classmethod
-    def get_interview_for_applicants(cls, object_list):
-        object_list = Applicant.new_applicant()
+    def get_interview_for_applicants(cls):
+        object_list = Applicant.get_applicants_without_interview()
         for i in object_list:
             break_boolean = False
             interview_slot_object_list = cls.get_mentor_object_list_not_reserved()
@@ -35,6 +34,8 @@ class Interview_slot(BaseModel):
                             j.save()
                             k.reserved = True
                             k.save()
+                            i.has_interview = True
+                            i.save()
                             Interview.create(
                                 date_time=j.start,
                                 mentor_1=j.mentor,
