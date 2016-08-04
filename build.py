1fbba468c7b62d48087ca57a45b29ff7c0bd6264
vbenedek <xbenedek01@gmail.com>
2016-08-04 17:48:28 +0200

    new mentorinterview model added

--- build.py
+++ build.py
@@ -8,8 +8,9 @@ from model_question import *
 from model_answer import *
 from model_mentor import *
 from model_interview_slot import *
+from model_mentorinterview import *
 
 db.connect()
 # List the tables here what you want to create...
-db.drop_tables([Applicant, City, School, Interview, Question, Answer, Mentor, InterviewSlot], safe=True, cascade=True)
-db.create_tables([Applicant, City, School, Interview, Question, Answer, Mentor, InterviewSlot], safe=True)
+db.drop_tables([Applicant, City, School, Interview, Question, Answer, Mentor, InterviewSlot, MentorInterview], safe=True, cascade=True)
+db.create_tables([Applicant, City, School, Interview, Question, Answer, Mentor, InterviewSlot, MentorInterview], safe=True)
