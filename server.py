bb21b9509d860f71a510bf2c17216f9b007ee092
vbenedek <xbenedek01@gmail.com>
2016-08-31 22:59:02 +0200

    empty slots validation implemented

--- server.py
+++ server.py
@@ -2,24 +2,30 @@ from flask import *
 from model_applicant import *
 from model_city import *
 
-
 app = Flask(__name__)
+app.config.update(dict(SECRET_KEY='development key'))
 
 
 @app.route('/registration', methods=['GET', 'POST'])
 def registration():
+    city_list = ["Choose a city!", "Kaposvár", "Érd", "Visegrád", "Veszprém", "Sopron", "Kecskemét", "Győr", "Gödöllő",
+                 " Hatvan", "Vác", "Eger", "Nyíregyháza", "Debrecen", "Kiskőrös", "Gyula", "Hajdúszoboszló", "Kassa",
+                 "Wieliczka", "Liszki", "Katowice", "Varsó", "Berlin"]
     if request.method == 'GET':
-        city_list = ["Kaposvár", "Érd", "Visegrád", "Veszprém", "Sopron", "Kecskemét", "Győr", "Gödöllő", " Hatvan",
-                     "Vác", "Eger", "Nyíregyháza", "Debrecen", "Kiskőrös", "Gyula", "Hajdúszoboszló", "Kassa",
-                     "Wieliczka", "Liszki", "Katowice", "Varsó", "Berlin"]
-        return render_template("registration.html", city_list=city_list)
+        return render_template("registration.html", city_list=city_list, form_data=request.form)
     elif request.method == 'POST':
         columns = ["first_name", "last_name", "city", "email"]
         a = [request.form[element] for element in columns]
-        c = City.select(City.id).where(City.name == a[2])
-        applicant = Applicant(first_name=a[0], last_name=a[1], city=c, email=a[3])
-        applicant.save()
-        return redirect(url_for('succesfull'))
+        if all(a) and request.form["city"] != "Choose":
+            print(request.form["city"])
+            c = City.select(City.id).where(City.name == a[2])
+            applicant = Applicant(first_name=a[0], last_name=a[1], city=c, email=a[3])
+            applicant.save()
+            return redirect(url_for('succesfull'))
+        else:
+            flash("Please fill!")
+            return render_template("registration.html", city_list=city_list, form_data=request.form)
+
 
 @app.route('/registration/succesfull')
 def succesfull():
